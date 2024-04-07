0704ace12b1a4c1c9e7250fcf9a60424cddb2d6b
qwetry4444 <mishkin_mi@edu.surgu.ru>
2024-04-07 20:51:41 +0500

    fix

--- TreeLab5/Node.cs
+++ TreeLab5/Node.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TreeLab5
 {
@@ -10,24 +11,20 @@ namespace TreeLab5
     {
         public Node<T> Root { get; set; }
 
-        public Node<T> InsertNode(Node<T> node, Node<T> currentNode)
+        public Tree(Node<T> root)
         {
-            if (Root == null)
-            {
-                return Root = node;
-            }
+            Root = root;
+        }
 
-            currentNode = currentNode ?? Root;
-            int result;
-            return (result = node.Data.CompareTo(currentNode.Data)) == 0
-                ? currentNode
-                : result < 0
-                    ? (currentNode.LeftNode == null)
-                        ? (currentNode.LeftNode = node)
-                        : InsertNode(node, currentNode.LeftNode)
-                    : (currentNode.RightNode == null)
-                        ? (currentNode.RightNode = node)
-                        : InsertNode(node, currentNode.RightNode);
+        public Node<T> FindMin(Node<T>? startWithNode = null)
+        {
+            startWithNode = startWithNode ?? Root;
+            if (startWithNode.LeftNode != null)
+            {
+                return FindMin(startWithNode.LeftNode);
+            }
+            return startWithNode;
+
         }
 
         public Node<T>? FindNode(T data, Node<T>? startWithNode = null)
@@ -42,16 +39,207 @@ namespace TreeLab5
                         : FindNode(data, startWithNode.LeftNode)
                     : startWithNode.RightNode == null
                         ? null
-                        : FindNode(data, startWithNode.RightNode);
+                        : FindNode(data, startWithNode.RightNode);    
+        }
+
+        public Node<T>? DeleteLeafNode(Node<T> node)
+        {
+            return (node.ParentNode.LeftNode == node)
+                ? (node.ParentNode.LeftNode = null)
+                : (node.ParentNode.RightNode == node)
+                    ? (node.ParentNode.RightNode = null)
+                    : null;
+        }
+
+        public Node<T>? DeleteOneChildNode(Node<T> node)
+        {
+            return (node.ParentNode.LeftNode == node)
+                ? node.LeftNode == null
+                    ? node.ParentNode.LeftNode = node.RightNode
+                    : node.ParentNode.LeftNode = node.LeftNode
+                : node.ParentNode.RightNode == node
+                    ? node.LeftNode == null
+                        ? node.ParentNode.RightNode = node.RightNode
+                        : node.ParentNode.RightNode = node.LeftNode
+                    : null;
+        }
+
+        public Node<T>? DeleteNode(Node<T>? startWithNode, Node<T> nodeToDelete)
+        {
+            if (startWithNode == null)
+                return startWithNode;
+
+            if (nodeToDelete.Data.CompareTo(startWithNode.Data) < 0)
+            {
+                startWithNode.LeftNode = DeleteNode(startWithNode.LeftNode, nodeToDelete);
+            }
+            else if (nodeToDelete.Data.CompareTo(startWithNode.Data) > 0)
+            {
+                startWithNode.RightNode = DeleteNode(startWithNode.RightNode, nodeToDelete);
+            }
+            else
+            {
+                if (startWithNode.LeftNode == null)
+                {
+                    return startWithNode.LeftNode;
+                }
+                else if (startWithNode.RightNode == null)
+                {
+                    return startWithNode.RightNode;
+                }
+
+                Node<T> minRigthNode = FindMin(Root.RightNode);
+                Root = minRigthNode;
+                Root.RightNode = DeleteNode(Root.RightNode, minRigthNode);
+
+            }
+            return Root;
+        }
+
+        public void printBT(string prefix, Node<T>? node, bool isLeft)
+        {
+            if(node != null )
+            {
+                Console.Write(prefix);
+
+                Console.Write(isLeft? "├──" : "└──" );
+
+                Console.WriteLine(node.Data);
+
+                printBT(prefix + (isLeft? "│   " : "    "), node.LeftNode, true);
+                printBT(prefix + (isLeft? "│   " : "    "), node.RightNode, false);
+            }
+        }
+
+        public void printBT(Node<T> node)
+        {
+            printBT("", node, false);
+        }
+
+public int NodeHeight(Node<T>? startWithNode)
+{
+    int left, right, height;
+    height = 0;
+    if (startWithNode != null)
+    {
+        left = NodeHeight(startWithNode.LeftNode);
+        right = NodeHeight(startWithNode.RightNode);
+        height = (left > right ? left : right) + 1;
+    }
+    return height;
+}
+
+
+        public int CountNodesTree(Node<T>? startWithNode)
+        {
+            if (startWithNode == null)
+                return 0;
+
+            int leftCount, rightCount;
+
+            leftCount = CountNodesTree(startWithNode.LeftNode);
+            rightCount = CountNodesTree(startWithNode.RightNode);
+
+            return leftCount + rightCount + 1;
+        }
+
+
+        public Node<T> RotateRight(Node<T> p) 
+        {
+            Node<T>? q = p.LeftNode;
+            if (q == null) return p;
+            p.LeftNode = q.RightNode;
+            q.RightNode = p;
+            return q;
+        }
+
+        public Node<T> RotateLeft(Node<T> q)
+        {
+            Node<T>? p = q.RightNode;
+            if (p == null) return q;
+            q.RightNode = p.LeftNode;
+            p.LeftNode = q;
+            return p;
+        }
+
+        public Node<T> InsertNode(Node<T> nodeToInsert, Node<T>? startWithNode)
+        {
+            if (startWithNode == null)
+            {
+                return nodeToInsert;
+            }
+
             
+            if (nodeToInsert.Data.CompareTo(startWithNode.Data) < 0)
+            {
+                startWithNode.LeftNode = InsertNode(nodeToInsert, startWithNode.LeftNode);
+            }
+            else
+            {
+                startWithNode.RightNode = InsertNode(nodeToInsert, startWithNode.RightNode);
+            }
+            return startWithNode;
+        }
+
+
+        public Node<T> InsertRoot(Node<T> nodeToInsert, Node<T>? startWithNode = null)
+        {
+            if (startWithNode == null) return nodeToInsert;
+            if (nodeToInsert.Data.CompareTo(startWithNode.Data) < 0)
+            {
+                startWithNode.LeftNode = InsertRoot(nodeToInsert, startWithNode.LeftNode);
+                return Root = RotateRight(startWithNode);
+            }
+            else
+            {
+                startWithNode.RightNode = InsertRoot(nodeToInsert, startWithNode.RightNode);
+                return Root = RotateLeft(startWithNode);
+            }
+        }
+
+
+public Node<T> RandomInsert(Node<T> nodeToInsert, Node<T>? startWithNode = null)
+{
+    Random rnd = new Random();
+    if (startWithNode == null) return nodeToInsert;
+    if (rnd.Next(0, CountNodesTree(startWithNode) + 1) == 1)
+    {
+        return InsertRoot(nodeToInsert, startWithNode);
+    }
+    if (nodeToInsert.Data.CompareTo(startWithNode.Data) < 0)
+    {
+        return InsertNode(nodeToInsert, startWithNode.LeftNode);
+    }
+    else
+    {
+        return InsertNode(nodeToInsert, startWithNode.RightNode);
+    }
+}
+
+        public int GetSumLengthPathToEvens(Node<int> startWithNode, int currentLength = 0)
+        {
+            if (startWithNode == null) return 0;
+
+            int left, right;
+
+            left = GetSumLengthPathToEvens(startWithNode.LeftNode, currentLength + 1);
+            right = GetSumLengthPathToEvens(startWithNode.RightNode, currentLength + 1);
+
+            if (startWithNode.Data % 2 == 0)
+            {
+                return left + right + currentLength;
+            }
+            return left + right;
         }
     }
+
+
 
     public class Node<T> where T : IComparable
     {
         public T Data { get; set; }
-        public Node<T> LeftNode { get; set; }
-        public Node<T> RightNode { get; set; }
+        public Node<T>? LeftNode { get; set; }
+        public Node<T>? RightNode { get; set; }
         public Node<T> ParentNode { get; set; }
 
         public Node(T data)
--- TreeLab5/Program.cs
+++ TreeLab5/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace TreeLab5
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //Node<int> node1 = new Node<int>(1);
+            //Node<int> node2 = new Node<int>(2);
+            //Node<int> node3 = new Node<int>(3);
+            //Node<int> node4 = new Node<int>(4);
+            //Node<int> node5 = new Node<int>(50000);
+
+            Random rnd = new Random();
+
+
+            //for (int countNodes = 1; countNodes < 6; countNodes++)
+            //{
+            //    Tree<int> tree = new Tree<int>(new Node<int>(rnd.Next(0, countNodes * 10000)));
+
+            //    for (int i = 2; i < countNodes * 10000; i++)
+            //    {
+            //        tree.InsertNode(new Node<int>(rnd.Next(0, 10000)), tree.Root);
+            //    }
+
+            //    Console.WriteLine($"Количество вершин: {countNodes * 10000} Высота дерева: {tree.NodeHeight(tree.Root)}");
+            //}
+            //Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+            //for (int countNodes = 1; countNodes < 6; countNodes++)
+            //{
+            //    Tree<int> tree = new Tree<int>(new Node<int>(rnd.Next(0, 10000)));
+
+            //    for (int i = 2; i < countNodes * 10000; i++)
+            //    {
+            //        tree.RandomInsert(new Node<int>(rnd.Next(0, countNodes * 10000)), tree.Root);
+            //    }
+
+            //    Console.WriteLine($"Количество вершин: {countNodes * 10000} Высота дерева: {tree.NodeHeight(tree.Root)}");
+            //}
+
+            Tree<int> tree = new Tree<int>(new Node<int>(5));
+            ////tree.InsertRoot(node1, tree.Root);
+            //tree.InsertRoot(new Node<int>(1), tree.Root);
+            //tree.InsertNode(new Node<int>(11), tree.Root);
+            //tree.InsertNode(new Node<int>(6), tree.Root);
+            //tree.InsertNode(new Node<int>(-100), tree.Root);
+
+            //tree.printBT(tree.Root);
+
+            for (int i = 2; i < 10; i++)
+            {
+                tree.InsertNode(new Node<int>(i), tree.Root);
+            }
+            tree.printBT(tree.Root);
+            Console.WriteLine(tree.GetSumLengthPathToEvens(tree.Root));
+
+            //Console.WriteLine($"Количество вершин: {20000} Высота дерева: {tree.NodeHeight(tree.Root)}");
+
+
+
+        }
+    }
+}
